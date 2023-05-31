9dc9fcc77e2e827d2c5ba512f10d3c8dd2eb201f
simretB05 <happysimret@gmail.com>
2023-05-30 19:39:58 -0600

    imported a module

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 import random
-import antigravity 
+# import antigravity 
+import helpers.log
+import wikipedia
+try:
+    user_input=input('which page do you want to see?')
+
+    result=wikipedia.page(user_input)
+    print(result.title)
+    print(result.content) 
+except PageError(title):
+  print('please add a correct title')
+except:
+   print('something went wrong')
 
 my_random_num= random.randint(-100,100)
 if(my_random_num>=50):
     print('Winner')
 else:
     print('loser')
-    import antigravity 
+
+helpers.log.addLogging('this')
+helpers.log.addLogging('is')
+helpers.log.addLogging('new')
+
+
 
---
+++ helpers/log.py
@@ -0,0 +1,4 @@
+def addLogging(str):
+    print('LOGGING',str)
+
+
